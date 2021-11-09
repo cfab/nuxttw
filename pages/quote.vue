b8d192bd3e41d04dee3c7695cf7168afbb800bce
Fabrice Cortat <fab@wonderweb.ch>
2021-11-09 14:18:59 +0100

    used links

--- pages/quote.vue
+++ pages/quote.vue
@@ -8,11 +8,14 @@
     </div>
 </template>
 <script setup>
+// https://www.youtube.com/watch?v=lLr_GZo5xZw
+// https://www.youtube.com/watch?v=ClF9BvKCVwY
 const { data: quote } = await useFetch('https://api.quotable.io/random')
 
 let { $config } = useNuxtApp()
 console.log($config.myURL)
 let test = await fetch(`${$config.myURL}/api/quote`)
 test = await test.json()
+
 const apiQuote = test.apiQuote
 </script>
