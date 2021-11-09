13bac724d533ec389f8201915ebbf8483a45d23f
Fabrice Cortat <fab@wonderweb.ch>
2021-11-09 14:39:16 +0100

    simpler calls

--- pages/quote.vue
+++ pages/quote.vue
@@ -2,20 +2,20 @@
     <div>
         <h1>Quote</h1>
         <br>
-       <div v-if="quote"> {{quote}}</div>
+       <div v-if="quote"> {{ quote }}</div>
         <br>
-       <div v-if="apiQuote"> {{apiQuote}}</div>
+       <div v-if="apiQuote"> {{ apiQuote }}</div>
     </div>
 </template>
 <script setup>
 // https://www.youtube.com/watch?v=lLr_GZo5xZw
 // https://www.youtube.com/watch?v=ClF9BvKCVwY
+
+// direct call
 const { data: quote } = await useFetch('https://api.quotable.io/random')
+// call through server/api
+let { myURL} = useRuntimeConfig()
+let test = await fetch(`${myURL}/api/quote`)
+const { apiQuote } = await test.json()
 
-let { $config } = useNuxtApp()
-console.log($config.myURL)
-let test = await fetch(`${$config.myURL}/api/quote`)
-test = await test.json()
-
-const apiQuote = test.apiQuote
 </script>
